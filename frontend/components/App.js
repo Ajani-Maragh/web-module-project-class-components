94019b9ecabd6982ccf23777c149a01218f708ec
Ajani Maragh <amsbstart@gmail.com>
2024-09-02 21:00:21 -0700

    finished app and todoList components

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,11 +1,60 @@
 import React from 'react'
+import TodoList from './TodoList'
+import Form from './Form'
 
 export default class App extends React.Component {
-  render() {
-    return (
+  constructor() {
+    super(); 
+    this.state = {
+      todos: [],
+      task: ''
+    }
+    }
+    handleInputChange = (event) => {
+      this.setState({ task: event.target.value })
+    }
+    handleAddTodo = (event) => {
+      event.preventDefault();
+      const newTodo = {
+        task: this.state.task,
+        id: Date.now(),
+        completed: false,
+      }
+      this.setState((prevState) => ({
+        todos: [...prevState.todos, newTodo],
+        task: '',
+      }))
+    }
+    toggleTodoComplete = (id) => {
+      this.setState((prevState) => ({
+        todos: prevState.todos.map((todo) =>
+        todo.id === id ? {...todo, completed: !todo.completed} : todo
+        )
+      }))
+    }
+    clearCompleted = () => {
+      this.setState((prevState) => ({
+        todos: prevState.todos.filter((todo) => !todo.completed),
+      }))
+    }
+
+    render() {
+      return (
       <div>
-        Todo App
+        
+        <Form 
+        task={this.state.task}
+        handleInputChange={this.handleInputChange}
+        handleAddTodo={this.handleAddTodo}
+        clearCompleted={this.clearCompleted}
+        />
+        <TodoList 
+        todos={this.state.todos}
+        toggleTodoComplete={this.toggleTodoComplete}
+        />
       </div>
-    )
+    );
   }
 }
+
+
--- frontend/components/TodoList.js
+++ frontend/components/TodoList.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import Todo from './Todo'
 
 export default class TodoList extends React.Component {
   render() {
     return (
       <div>
-        TodoList
+        {this.props.todos.map((todo) => (
+          <Todo 
+          key={todo.id}
+          todo={todo}
+          toggleCompleted={this.props.toggleCompleted}
+          />
+        ))}
       </div>
     )
   }
